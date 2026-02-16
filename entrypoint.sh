a867effbcd8faeffde2ce78139a54700f1ba8010
Dave Horton <daveh@beachdognet.com>
2026-02-16 16:25:05 -0500

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -70,10 +70,6 @@ fi
 if [ -z "$LOGLEVEL" ]; then
   LOGLEVEL=5
 fi
-if [ -z "$HTTP_PORT" ]; then
-  HTTP_PORT=8080
-fi
-
 echo "LOGLEVEL is $LOGLEVEL"
 
 if [ "$1" = 'rtpengine' ]; then
@@ -82,7 +78,7 @@ if [ "$1" = 'rtpengine' ]; then
   --interface ${PRIVATE_INTERFACE} --interface ${PUBLIC_INTERFACE} \
   --port-min ${RTP_START_PORT} --port-max ${RTP_END_PORT} \
   --log-level ${LOGLEVEL} --port-min ${RTP_START_PORT} --port-max ${RTP_END_PORT} \
-  --listen-ng=22222 --listen-http=${HTTP_PORT} --listen-udp=12222 \
+  --listen-ng=22222 --listen-udp=12222 \
   --dtmf-log-dest=127.0.0.1:22223 \
   --listen-cli=127.0.0.1:9900 \
   --pidfile /var/run/rtpengine.pid \
